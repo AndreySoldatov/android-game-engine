6874f8e2b851cc1c9340c0eeefb86e71f3ae7480
Andrey Soldatov <qwer4132qwer@gmail.com>
2026-06-16 12:00:10 +0300

    Continue work on virtual thumbstick widget. Added radial and length snapping and polished the demo.

--- vcon-widgets/examples/demo.rs
+++ vcon-widgets/examples/demo.rs
@@ -1,23 +1,59 @@
-use egui::{Rect, Slider, Vec2};
-use vcon_widgets::thumbstick::Thumbstick;
+use std::f32::consts::PI;
+
+use egui::{Checkbox, Rect, Slider, Vec2};
+use vcon_widgets::thumbstick::{RadialSnap, Thumbstick};
+
+struct ThumbstickState {
+    v: Vec2,
+    ir: f32,
+    or: f32,
+    ror: bool,
+}
 
 fn main() {
-    let mut v = Vec2::ZERO;
-    let mut pos = Vec2::splat(50.0);
+    let mut thumb_state = ThumbstickState {
+        v: Vec2::ZERO,
+        ir: 20.0,
+        or: 80.0,
+        ror: true,
+    };
+    let mut do_ls = false;
+    let mut ls = 4;
+
+    let mut do_rs = false;
+    let mut rs = 4;
+    let mut offset = 0.0;
 
     eframe::run_ui_native(
         "vcon-widgets collection",
         eframe::NativeOptions::default(),
         move |ui, _frame| {
             egui::CentralPanel::default().show_inside(ui, |ui| {
-                ui.label(format!("{:.2?}", v));
+                ui.label(format!("Thumbstick value: {:.2?}", thumb_state.v));
 
-                ui.add(Slider::new(&mut pos.x, 50.0..=150.0));
-                ui.add(Slider::new(&mut pos.y, 50.0..=150.0));
+                ui.add(Slider::new(&mut thumb_state.ir, 1.0..=150.0));
+                ui.add(Slider::new(&mut thumb_state.or, 1.0..=150.0));
 
-                ui.place(
-                    Rect::from_center_size((pos * 2.0).to_pos2(), Vec2::splat(40.0 + 80.0)),
-                    Thumbstick::new(&mut v),
+                ui.checkbox(&mut thumb_state.ror, "Reset on release");
+
+                ui.checkbox(&mut do_ls, "Length snap");
+                ui.add(Slider::new(&mut ls, 2..=8));
+
+                ui.checkbox(&mut do_rs, "Radial snap");
+                ui.add(Slider::new(&mut rs, 2..=8));
+                ui.add(Slider::new(&mut offset, 0.0..=PI));
+
+                ui.add(
+                    Thumbstick::new(&mut thumb_state.v)
+                        .inner_radius(thumb_state.ir)
+                        .outer_radius(thumb_state.or)
+                        .reset_on_release(thumb_state.ror)
+                        .length_steps(if do_ls { Some(ls) } else { None })
+                        .radial_snap(if do_rs {
+                            Some(RadialSnap { steps: rs, offset })
+                        } else {
+                            None
+                        }),
                 );
             });
         },
--- vcon-widgets/src/thumbstick.rs
+++ vcon-widgets/src/thumbstick.rs
@@ -1,10 +1,46 @@
-use egui::{Sense, Stroke, Vec2};
+use std::f32::consts::PI;
 
+use egui::{Pos2, Sense, Stroke, Vec2};
+
+#[derive(Clone, Copy)]
+pub struct RadialSnap {
+    /// The number of steps for thumbstick angle snapping.
+    ///
+    /// Must be in 2..8 range
+    pub steps: usize,
+    /// The offset angle in radians for thumbstick angle snapping.
+    ///
+    /// Must be in 0..=step_size range,
+    /// where step_size is (2.0 * PI) / steps.
+    pub offset: f32,
+}
+
+/// A widget that allows to control a 2D vector value using a thumbstick.
+///
+/// The thumbstick is a circle with a smaller circle inside it. The user can drag the inner circle to control the value.
+///
+/// The value is normalized to the range [-1, 1] for both x and y axes.
 pub struct Thumbstick<'a> {
+    /// The value to be controlled by the thumbstick.
     value: &'a mut Vec2,
+    /// The radius of the inner circle.
     inner_radius: f32,
+    /// The radius of the outer circle.
     outer_radius: f32,
+    /// The dead zone of the thumbstick. If the value is within the dead zone, it will be set to zero.
     dead_zone: Option<f32>,
+    /// If true, the value will be reset to zero when the user releases the thumbstick.
+    reset_on_release: bool,
+    /// If this value is some snaps the length of the thumbstick value to a closest step.
+    ///
+    /// This value must be in 2..=8 range
+    length_steps: Option<usize>,
+    /// If this value is some snaps the angle of the thumbstick value to a closest step.
+    ///
+    /// The step count must be in 2..=8 range.
+    ///
+    /// The offset angle must be in 0..=step_size range.
+    radial_snap: Option<RadialSnap>,
 }
 
 impl<'a> Thumbstick<'a> {
@@ -14,6 +50,9 @@ impl<'a> Thumbstick<'a> {
             inner_radius: 40.0,
             outer_radius: 80.0,
             dead_zone: None,
+            reset_on_release: true,
+            length_steps: None,
+            radial_snap: None,
         }
     }
 
@@ -23,12 +62,51 @@ impl<'a> Thumbstick<'a> {
     }
 
     pub fn inner_radius(mut self, inner_radius: f32) -> Self {
+        assert!(
+            inner_radius > 0.0 && inner_radius <= self.outer_radius,
+            "Inner radius of the thumbstick must be greater than zero and less or equal than outer radius"
+        );
         self.inner_radius = inner_radius;
         self
     }
 
     pub fn outer_radius(mut self, outer_radius: f32) -> Self {
+        assert!(
+            outer_radius > 0.0 && outer_radius >= self.inner_radius,
+            "Outer radius of the thumbstick must be greater than zero and greater or equal than inner radius"
+        );
         self.outer_radius = outer_radius;
+        self
+    }
+
+    pub fn reset_on_release(mut self, reset_on_release: bool) -> Self {
+        self.reset_on_release = reset_on_release;
+        self
+    }
+
+    pub fn length_steps(mut self, length_steps: Option<usize>) -> Self {
+        if let Some(length_steps) = length_steps {
+            assert!(
+                length_steps >= 2 && length_steps <= 8,
+                "Length steps of the thumbstick must be greater or equal than 2 and less or equal than 8"
+            );
+        }
+        self.length_steps = length_steps;
+        self
+    }
+
+    pub fn radial_snap(mut self, radial_snap: Option<RadialSnap>) -> Self {
+        if let Some(radial_snap) = radial_snap {
+            assert!(
+                radial_snap.steps >= 2 && radial_snap.steps <= 8,
+                "Radial snap steps of the thumbstick must be greater or equal than 2 and less or equal than 8"
+            );
+            assert!(
+                radial_snap.offset <= ((2.0 * PI) / radial_snap.steps as f32),
+                "Offset must be less or equal step size"
+            )
+        }
+        self.radial_snap = radial_snap;
         self
     }
 }
@@ -60,11 +138,32 @@ impl<'a> egui::Widget for Thumbstick<'a> {
                 }
             }
 
+            if let Some(length_steps) = self.length_steps {
+                let l = v.length();
+                let closest_step =
+                    (l * (length_steps - 1) as f32).round() / (length_steps - 1) as f32;
+                let scale = closest_step / l;
+
+                v *= scale;
+            }
+
+            if let Some(radial_snap) = self.radial_snap {
+                let a = v.y.atan2(v.x);
+                let step_size = (2.0 * PI) / radial_snap.steps as f32;
+                let k = ((a - radial_snap.offset) / step_size).round();
+                let newa = radial_snap.offset + k * step_size;
+
+                let l = v.length();
+
+                v.x = newa.cos() * l;
+                v.y = newa.sin() * l;
+            }
+
             *self.value = v;
         }
 
         // Set to zero when released
-        if response.drag_stopped() {
+        if self.reset_on_release && response.drag_stopped() {
             *self.value = Vec2::ZERO;
         }
 
@@ -80,6 +179,29 @@ impl<'a> egui::Widget for Thumbstick<'a> {
                 visuals.bg_fill,
                 Stroke::new(1.0, visuals.bg_stroke.color),
             );
+
+            if let Some(length_steps) = self.length_steps {
+                for i in 1..(length_steps - 1) {
+                    let ratio = i as f32 / (length_steps - 1) as f32;
+                    painter.circle_stroke(
+                        center,
+                        self.outer_radius * ratio,
+                        Stroke::new(1.0, visuals.fg_stroke.color.gamma_multiply(0.5)),
+                    );
+                }
+            }
+
+            if let Some(radial_snap) = self.radial_snap {
+                for i in 0..radial_snap.steps {
+                    let a = (i as f32 / radial_snap.steps as f32) * 2.0 * PI + radial_snap.offset;
+                    let x = a.cos() * self.outer_radius;
+                    let y = a.sin() * self.outer_radius;
+                    painter.line_segment(
+                        [center, center + Vec2::new(x, y)],
+                        Stroke::new(1.0, visuals.fg_stroke.color.gamma_multiply(0.5)),
+                    );
+                }
+            }
 
             let knob_center = center + *self.value * self.outer_radius;
             painter.circle_filled(knob_center, self.inner_radius, visuals.fg_stroke.color);
